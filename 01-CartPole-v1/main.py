20cf768be3330be35bd0913e732d9f904c74e539
Kinda Lam <lamkinun@gmail.com>
2020-11-17 12:22:24 +0800

    CartPole-v1

--- 01-CartPole-v1/main.py
+++ 01-CartPole-v1/main.py
@@ -25,6 +25,7 @@ def run(env, agent, fps=60, is_render=True, is_train=False):
         if is_train:
             agent.remember(state, action, next_state, reward, done)
             agent.replay()
+            agent.update_target_network()
         state = next_state
 
         while time.time() - t1 < 1.0 / fps:
@@ -42,22 +43,24 @@ if __name__ == "__main__":
         in_shape=(_env.observation_space.shape[0],),
         out_size=_env.action_space.n,
         memory_size=10000, min_memory_size=1000,
-        epsilon=1.0, epsilon_decay=0.99995, epsilon_min=0.1,
+        epsilon=1.0, epsilon_decay=0.99995, epsilon_min=0.0,
         learning_rate=0.00025, batch_size=32
     )
-    _is_train = True
+    _is_train = False
 
     if _is_train:
         _history = []
         _agent.compile_model()
-        for _i in range(500):
+        for _i in range(1000):
             _score, _frames = run(_env, _agent, is_render=False, is_train=True)
+            if _i % 100 == 99:
+                _agent.save_weights("model-%d.h5" % (_i + 1))
             _agent.save_weights("model.h5")
 
             _history.append(_frames)
             with open("log.txt", "a+") as f:
-                f.write("{}\t{}\t{}\t{}\n".format(_i, _score, _frames, _agent.epsilon))
-            print("Epoch: {}, score: {}, frames: {}, epsilon: {}".format(_i, _score, _frames, _agent.epsilon))
+                f.write("{},{},{},{}\n".format(_i, _score, _frames, _agent.epsilon))
+            print("Episode: {}, Score: {}, Frames: {}, Epsilon: {}".format(_i, _score, _frames, _agent.epsilon))
         _env.close()
 
         plt.plot(range(len(_history)), _history)
@@ -65,7 +68,7 @@ if __name__ == "__main__":
         plt.show()
     else:
         _agent.epsilon = 0.0
-        _agent.load_weights("model.h5")
+        _agent.load_weights("model-1000.h5")
         _score = run(_env, _agent, is_render=True, is_train=False)
         print("Score: {}".format(_score))
 
